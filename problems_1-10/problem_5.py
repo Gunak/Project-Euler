4c0afaf3d43ca76a449ff22d15d94a9d36bf3173
= <GunaksGit@gmail.com>
2019-04-02 13:40:51 -0700

    solved problem 5

--- problems_1-10/problem_5.py
+++ problems_1-10/problem_5.py
@@ -3,5 +3,23 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
-def divisible_1_to_20():
-	pass
+def divisible_1_to_20(number):
+	works = True
+	for i in range(1, 21):
+		# print(number % i)
+		if number % i != 0:
+			works = False
+			return False
+	return number
+
+
+counter = 2520
+while True:
+	x = divisible_1_to_20(counter)
+	# print(x is False)
+	if x is False:
+		counter +=1
+	else:
+		break
+
+print(counter)
