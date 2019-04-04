5f89bfc67c75341f1ce3b7549b87e27e1a83135d
BuildTools <unconfigured@null.spigotmc.org>
2019-04-04 10:02:16 -0700

    solved problem 7

--- problems_1-10/problem_7.py
+++ problems_1-10/problem_7.py
@@ -3,3 +3,31 @@
 # What is the 10,001st prime number?
 
 
+def prime_number(number):
+	if number == 1:
+		return False
+	elif number == 2:
+		return number
+	elif number % 2 == 0:
+		return False
+	else:
+		for i in range(3, number, 2):
+			# print(f'\t {i}')
+			if number % i == 0 or i > number:
+				return False
+		else:
+			return number
+
+
+
+counter = 0
+primes_found = 0
+
+while primes_found < 10001:
+	counter += 1
+	x = prime_number(counter)
+	# print(x)
+	if x != False:
+		primes_found += 1
+
+print(prime_number(counter))
