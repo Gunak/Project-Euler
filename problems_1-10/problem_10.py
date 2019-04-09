3c8b63383e4bbc4f39948ff7ad484b52296630df
BuildTools <unconfigured@null.spigotmc.org>
2019-04-09 09:23:49 -0700

    soved problem 10

--- problems_1-10/problem_10.py
+++ problems_1-10/problem_10.py
@@ -3,3 +3,31 @@
 # Find the sum of all the primes below two million.
 
 
+def prime_number(number):
+	if number == 1:
+		return False
+	elif number == 2:
+		return number
+	elif number % 2 == 0:
+		return False
+	else:
+		for i in range(3, number, 2):
+			# print(f'\t {i}')
+			if number % i == 0 or i > number:
+				return False
+		else:
+			return number
+
+
+total = 0
+for i in range(1,10, 2):
+	print(i)
+	x = prime_number(i)
+	total += x
+
+	
+	# if x != False:
+	# 	print(x)
+	# 	total += x
+
+print(total)
